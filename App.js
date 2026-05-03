341306722a39d88940aa7f94edf16f06717b1501
rdp5rss-jpg <rdp5rss@gmail.com>
2026-05-03 21:32:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,68 +1,9 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [list, setList] = useState([]);
-
-  const addItem = () => {
-    if (text.trim() !== '') {
-      setList([...list, text]);
-      setText('');
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Todo App 🔥</Text>
-
-      <TextInput
-        placeholder="Enter task"
-        value={text}
-        onChangeText={setText}
-        style={styles.input}
-      />
-
-      <TouchableOpacity style={styles.button} onPress={addItem}>
-        <Text style={styles.btnText}>Add</Text>
-      </TouchableOpacity>
-
-      {list.map((item, index) => (
-        <Text key={index} style={styles.item}>• {item}</Text>
-      ))}
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>🔥 Working APK App</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    marginTop: 40,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 8,
-  },
-  button: {
-    backgroundColor: 'green',
-    padding: 12,
-    borderRadius: 10,
-    marginBottom: 20,
-  },
-  btnText: {
-    color: '#fff',
-    textAlign: 'center',
-  },
-  item: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-});
